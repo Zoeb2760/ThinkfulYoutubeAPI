5e29b151658ec08bdb35ae90c7103432645db5a4
ZB <Zoeb2760@gmail.com>
2015-10-25 15:39:46 -0400

    updated to include thumbnails

--- js/app1.js
+++ js/app1.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
-// declare global variables
+
 
 
 $('#search-Term').submit(function(event) {
+
+
+
     event.preventDefault();
     var searchTerm = $('#query').val();
     getRequest(searchTerm);
@@ -13,7 +16,7 @@ $('#search-Term').submit(function(event) {
         console.log(searchTerm);
         var params = {
             part: 'snippet',
-            //key: 'AIzaSyBKACb8CHAV4Lr1v4uq65MCdmm-HglaapI'
+            
 
             s: searchTerm,
             //q: 'dogs',
@@ -21,28 +24,31 @@ $('#search-Term').submit(function(event) {
             key: 'AIzaSyBKACb8CHAV4Lr1v4uq65MCdmm-HglaapI'
         };
         url = 'https://www.googleapis.com/youtube/v3/search';
-        //key: 'AIzaSyBKACb8CHAV4Lr1v4uq65MCdmm-HglaapI';
+        
 
 
         $.getJSON(url, params, function(data) {
+            //var myData= data.items;
             var myData= data.items;
-            //console.log(myData.snippet);
-            //var item = results.items;
-            console.log(myData);
+            
+            console.log("myData=" + myData);
             showResults(myData);
-            //searchByKeyword();
+            
             
         });
     }
     
 function showResults(results) {
-    //var title = snippet.channelTitle; 
+    
         
     var htmlresults = "";
     $.each(results, function(index, value) {
-    
-        htmlresults += '<p>' + value.snippet.title + '</p>';
-        console.log( value.snippet.title );
+
+        var snippeturl= value.snippet.thumbnails.medium.url;
+        console.log ("snippeturl= "+ value.snippet.thumbnails.medium.url);
+        htmlresults += '<p>' + value.snippet.title +   "<img src =" + snippeturl + ">" + '</p>';
+        console.log( value.snippet.thumbnails);
+        
     });
     $('#search-results').html(htmlresults);
 }
@@ -50,22 +56,7 @@ function showResults(results) {
 
 
   
-    /*function getRequest(seachTerm) {
-        $.getJSON("http://www.omdbapi.com/?s=" + searchTerm + "&r=json", function(data) {
-            showResults(data.Search);
-        });*/
-
-
-    
-//
-
-/*function searchByKeyword() {
-  var results = YouTube.Search.list('id,snippet', {q: 'dogs', maxResults: 25});
-  for(var i in results.items) {
-    var item = results.items[i];
-    Logger.log('[%s] Title: %s', item.id.videoId, item.snippet.title);
-  }
-}*/
+   
 
 
 
